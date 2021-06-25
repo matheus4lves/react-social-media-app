8edbf70419eaadac9cecd25a8483f323264f389b
Matheus Alves <matheusalves.bmd@gmail.com>
2021-06-25 17:38:32 -0300

    Work on details and implement composition
    Changes summary:
    
     - Change the title of the page according to its content
    
     - Apply composition between Page and Container. Notice that the when
       the About component is rendered it returns Page, which in turn
       returns Container. Notice that About passes its props to Page, which
       in turn passes its own props to Container. This is composition! In
       other words, components using components and passing props around as
       necessary.

---
+++ app/components/Page.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Container from "./Container";
+
+function Page(props) {
+  useEffect(() => {
+    document.title = `${props.title} | Social Media App`;
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Container>{props.children}</Container>
+    </>
+  );
+}
+
+export default Page;
