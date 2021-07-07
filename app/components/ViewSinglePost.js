29b265b415c67fa8310362bd468a0fd39ff83f5b
Matheus Alves <matheusalves.bmd@gmail.com>
2021-07-07 16:16:40 -0300

    Make the single post screen load posts from db

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,11 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Page from "./Page";
+import Axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ViewSinglePost() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`/post/${id}`);
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e.responde.data);
+      }
+    }
+
+    fetchPost();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Page title="...">
+        <div>Loading...</div>
+      </Page>
+    );
+  }
+
+  // After fetching a post, get its date and format it
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()}`;
+
   return (
-    <Page title="Fake hardcoded title.">
+    <Page title={post.title}>
       <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
+        <h2>{post.title}</h2>
         <span className="pt-2">
           <a href="#" className="text-primary mr-2" title="Edit">
             <i className="fas fa-edit"></i>
@@ -18,30 +52,12 @@ function ViewSinglePost() {
 
       <p className="text-muted small mb-4">
         <a href="#">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
+          <img className="avatar-tiny" src={post.author.avatar} />
         </a>
-        Posted by <a href="#">brad</a> on 2/10/2020
+        Posted by <a href="#">{post.author.username}</a> on {dateFormatted}
       </p>
 
-      <div className="body-content">
-        <p>
-          Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit.
-          Iure ea at esse, tempore qui possimus soluta impedit natus voluptate,
-          sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat
-          asperiores at.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod
-          asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab
-          exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore
-          qui possimus soluta impedit natus voluptate, sapiente saepe modi est
-          pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-        </p>
-      </div>
+      <div className="body-content">{post.body}</div>
     </Page>
   );
 }
