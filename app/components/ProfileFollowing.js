b4ce1ff6faa87244c4dc2f9ab01826c435928ad6
Matheus Alves <matheusalves.bmd@gmail.com>
2021-07-21 10:25:34 -0300

    Bring the Follower/Following profile tabs to life
    Let the user use these tabs to see who is following him as well as
    people he is following.

---
+++ app/components/ProfileFollowing.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { useParams, Link } from "react-router-dom";
+import LoadingDotsIcon from "./LoadingDotsIcon";
+
+function ProfileFollowing() {
+  const [isLoading, setIsLoading] = useState(true);
+  // The initial state is an empty array because the server will send
+  // an array with posts
+  const [posts, setPosts] = useState([]);
+  const { username } = useParams();
+
+  useEffect(() => {
+    const serverRequest = Axios.CancelToken.source();
+
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/following`, {
+          cancelToken: serverRequest.token,
+        });
+        setPosts(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e.response.data);
+      }
+    }
+
+    fetchPosts();
+
+    // cleanup function
+    return () => {
+      serverRequest.cancel();
+    };
+  }, [username]);
+
+  if (isLoading) return <LoadingDotsIcon />;
+
+  return (
+    <div className="list-group">
+      {posts.map((follower, index) => {
+        return (
+          <Link
+            key={index}
+            to={`/profile/${follower.username}`}
+            className="list-group-item list-group-item-action"
+          >
+            <img className="avatar-tiny" src={follower.avatar} />
+            {follower.username}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProfileFollowing;
