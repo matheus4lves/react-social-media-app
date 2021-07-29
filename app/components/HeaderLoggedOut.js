800d9fc39f76e36b066637ccb5e32ec54ef30a51
Matheus Alves <matheusalves.bmd@gmail.com>
2021-07-29 10:00:38 -0300

    Display login / logout flash messages

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -16,8 +16,16 @@ function HeaderLoggedOut(props) {
       });
       if (response.data) {
         appDispatch({ type: "login", data: response.data });
+        appDispatch({
+          type: "flashMessage",
+          value: "You have successfully logged in.",
+        });
       } else {
         console.log("Incorrect username/password.");
+        appDispatch({
+          type: "flashMessage",
+          value: "Invalid username / password.",
+        });
       }
     } catch (e) {
       console.log(e.response.data);
