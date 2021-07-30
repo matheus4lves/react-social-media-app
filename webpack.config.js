10ffe2acf2a0166e045da0b208ede13d7edb87b3
Matheus Alves <matheusalves.bmd@gmail.com>
2021-07-30 16:16:40 -0300

    Create a dist version of the project
    Config webpack to generate a dist folder and a script to allow to
    preview the project from this folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
 
-module.exports = {
+/*
+  Because I didn't bother making CSS part of our
+  webpack workflow for this project I'm just
+  manually copying our CSS file to the DIST folder. 
+*/
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy files", function () {
+      fse.copySync("./app/main.css", "./dist/main.css");
+
+      /*
+        If you needed to copy another file or folder
+        such as your "images" folder, you could just
+        call fse.copySync() as many times as you need
+        to here to cover all of your files/folders.
+      */
+    });
+  }
+}
+
+config = {
   entry: "./app/Main.js",
   output: {
     publicPath: "/",
     path: path.resolve(__dirname, "app"),
-    filename: "bundle.js",
+    filename: "bundled.js",
   },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "app/index-template.html",
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+  ],
   mode: "development",
-  devtool: "eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "app"),
-    port: 3000,
-    hot: true,
-    host: "0.0.0.0",
-    useLocalIp: true,
-    open: {
-      app: [
-        "/opt/firefox-84.0b4/firefox/firefox",
-        "-P web-development",
-        "--private-window",
-      ],
-    },
-    // tell the server to serve index.html in place of any 404 responses
-    historyApiFallback: true,
-  },
   module: {
     rules: [
       {
@@ -43,3 +61,36 @@ module.exports = {
     ],
   },
 };
+
+if (currentTask == "webpackDev" || currentTask == "dev") {
+  config.devtool = "eval-source-map";
+  config.devServer = {
+    contentBase: path.join(__dirname, "app"),
+    port: 3000,
+    hot: true,
+    host: "0.0.0.0",
+    useLocalIp: true,
+    open: {
+      app: [
+        "/opt/firefox-84.0b4/firefox/firefox",
+        "-P web-development",
+        "--private-window",
+      ],
+    },
+    // tell the server to serve index.html in place of any 404 responses
+    historyApiFallback: true,
+  };
+}
+
+if (currentTask == "webpackBuild") {
+  config.plugins.push(new CleanWebpackPlugin(), new RunAfterCompile());
+  config.mode = "production";
+  config.output = {
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+  };
+}
+
+module.exports = config;
